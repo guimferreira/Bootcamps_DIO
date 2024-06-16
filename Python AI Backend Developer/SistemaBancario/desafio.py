fb22a82caca73eb2f77dedb46da6498a38f3e88f
Guilherme de Medeiros Ferreira <g_medeiros@outlook.com.br>
2024-06-15 22:34:12 -0300

    Otimizando Sistema Bancário

--- Python AI Backend Developer/SistemaBancario/desafio.py
+++ Python AI Backend Developer/SistemaBancario/desafio.py
@@ -1,3 +1,69 @@
+def depositar(valor):
+    if valor > 0:
+        global saldo
+        saldo += valor
+        global extrato
+        extrato += f'Depósito: R$ {valor:.2f}\n'
+    else:
+        print('Operação falhou! O valor informado é inválido.')
+
+
+def efetuar_saque(valor):
+    global saldo
+    global extrato
+    global numero_saques
+    saldo -= valor
+    extrato += f'Saque: R$ {valor:.2f}\n'
+    numero_saques += 1
+
+
+def sacar(valor):
+    global saldo
+    global extrato
+    global limite
+    global numero_saques
+    global LIMITE_SAQUES
+
+    if valor > saldo:
+        print('Operação falhou! Você não tem saldo suficiente.')
+    elif valor > limite:
+        print('Operação falhou! O valor do saque excede o limite.')
+    elif numero_saques >= LIMITE_SAQUES:
+        print('Operação falhou! Número máximo de saques excedido.')
+    elif valor > 0:
+        efetuar_saque(valor)
+    else:
+        print('Operação falhou! O valor informado é inválido.')
+
+
+def emitir_extrato():
+    global saldo
+    global extrato
+
+    print(f"\n{'=' * 16} EXTRATO {'=' * 16}")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("=" * 42)
+
+
+def menu_index(op):
+    while True:
+        opcao = input(op)
+        match opcao:
+            case "d":
+                value = float(input("Informe o valor do depósito: "))
+                depositar(value)
+            case "s":
+                value = float(input("Informe o valor do saque: "))
+                sacar(value)
+            case "e":
+                emitir_extrato()
+            case "q":
+                break
+            case _:
+                print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
 menu = """
 
 [d] Depositar
@@ -13,55 +79,5 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 4
 
-while True:
-
-    opcao = input(menu)
-
-    match opcao:
-        case "d":
-            valor = float(input("Informe o valor do depósito: "))
-
-            if valor > 0:
-                saldo += valor
-                extrato += f"Depósito: R$ {valor:.2f}\n"
-
-            else:
-                print("Operação falhou! O valor informado é inválido.")
-
-        case "s":
-            valor = float(input("Informe o valor do saque: "))
-
-            excedeu_saldo = valor > saldo
-
-            excedeu_limite = valor > limite
-
-            excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-            if excedeu_saldo:
-                print("Operação falhou! Você não tem saldo suficiente.")
-
-            elif excedeu_limite:
-                print("Operação falhou! O valor do saque excede o limite.")
-
-            elif excedeu_saques:
-                print("Operação falhou! Número máximo de saques excedido.")
-
-            elif valor > 0:
-                saldo -= valor
-                extrato += f"Saque: R$ {valor:.2f}\n"
-                numero_saques += 1
-
-            else:
-                print("Operação falhou! O valor informado é inválido.")
-
-        case "e":
-            print(f"\n{'='*16} EXTRATO {'='*16}")
-            print("Não foram realizadas movimentações." if not extrato else extrato)
-            print(f"\nSaldo: R$ {saldo:.2f}")
-            print("="*42)
-
-        case "q":
-            break
-
-        case _:
-            print("Operação inválida, por favor selecione novamente a operação desejada.")
+opcao = input()
+menu_index(opcao)
